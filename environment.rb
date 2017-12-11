690f1ba1c22af3d901ed6997504f39547f01c341
Kristen Peck <kpeck71@gmail.com>
2017-12-11 04:14:22 +0000

    automatically backed up by learn

--- environment.rb
+++ environment.rb
@@ -3,10 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 require 'sinatra/base'
 require 'sinatra/reloader'
