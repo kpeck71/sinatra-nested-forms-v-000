21e03e24631a42e5011d575ec5c8f9477aa440c7
Kristen Peck <kpeck71@gmail.com>
2017-12-11 04:15:09 +0000

    automatically backed up by learn

--- app/models/ships.rb
+++ app/models/ships.rb
@@ -4,7 +4,11 @@ class Ships
 
   attr_accessor :name, :type, :booty
 
-  def initilialize (:name,)
+  def initilialize(name,type,booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
 
 
   def self.clear
